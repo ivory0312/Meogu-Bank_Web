be8e0edf90a1afabe73a9680a5b0046c83af69f6
ivory0312 <63035523+ivory0312@users.noreply.github.com>
2021-10-07 22:24:24 +0900

    Add: side bar profile, list

--- bank/src/components/Common/Side/Side.tsx
+++ bank/src/components/Common/Side/Side.tsx
@@ -1,7 +1,37 @@
+import profileImage from "assets/defaultProfile.svg";
+import Home from "assets/Home/Home";
+import Money from "assets/Money/Money";
+import { useHistory } from "react-router-dom";
+
 import "./Side.scss";
 
 const Side = (): JSX.Element => {
-  return <div className="side">side</div>;
+  const history = useHistory();
+  const path: string = window.location.pathname;
+
+  return (
+    <div className="side">
+      <div className="side-profile">
+        <img src={profileImage} alt="profileImage" />
+        <div className="side-profile-info">
+          <div>안녕하세요!</div>
+          <div>
+            <span className="side-profile-info-name">박상아</span> 님
+          </div>
+        </div>
+      </div>
+      <div className="side-list">
+        <div className="side-list-item">
+          <Home props={path === "/" ? "#4880EE" : "#999999"} />
+          <span>홈 화면</span>
+        </div>
+        <div className="side-list-item">
+          <Money props="#999999" />
+          <span>계좌 이체</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Side;
