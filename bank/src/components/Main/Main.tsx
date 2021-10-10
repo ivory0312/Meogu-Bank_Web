7475d98bed0c0b279e6407b062a0c572b069e44d
ivory0312 <63035523+ivory0312@users.noreply.github.com>
2021-10-11 01:42:46 +0900

    Add: add main list box

--- bank/src/components/Main/Main.tsx
+++ bank/src/components/Main/Main.tsx
@@ -1,64 +1,13 @@
-import { useState } from "react";
-import MainListItem from "./MainListItem";
-import useMainList from "hooks/main/useMainList";
+import MainList from "./MainList/MainList";
 
 import "./Main.scss";
 
 const Main = (): JSX.Element => {
-  const { MainListDumi } = useMainList();
-  const [isMore, setIsMore] = useState<boolean>(false);
-  const [height, setHeight] = useState<string>("335");
-  let count: number = 0;
-  const handleMore = () => {
-    if (isMore) {
-      setIsMore(false);
-      setHeight("335");
-      console.log(height);
-    } else {
-      setIsMore(true);
-      setHeight(String(85 * MainListDumi.length));
-      console.log(height);
-    }
-  };
-
   return (
     <>
       <div className="main">
-        <div className="main-list">
-          <div className="main-list-title">
-            <div>계좌</div>
-            <div
-              className="main-list-title-button"
-              onClick={() => handleMore()}
-            >
-              {isMore ? "간략히" : "자세히"}
-            </div>
-          </div>
-          <div className="main-list-content" style={{ height: `${height}px` }}>
-            {MainListDumi.map((data) => {
-              // if (count !== 4) {
-              //   count += 1;
-              // } else {
-              //   return;
-              // }
-              return (
-                <>
-                  <MainListItem name={data.name} price={data.price} />
-                </>
-              );
-            })}
-            {/* <div className={isMore ? "isMore" : "isntMore"}>
-            {MainListDumi.slice(4).map((data) => {
-              return (
-                <>
-                <MainListItem name={data.name} price={data.price} />
-                </>
-                );
-              })}
-              <div onClick={() => handleMore()}>간략히</div>
-            </div> */}
-          </div>
-        </div>
+        <MainList title="계좌" content="계좌를 개설해보세요" />
+        <MainList title="저축" content="저축을 시작해보세요" />
       </div>
     </>
   );
---
+++ bank/src/components/Main/MainList/MainList.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import MainListItem from "components/Main/MainList/MainListItem";
+import useMainList from "hooks/main/useMainList";
+import Add from "assets/add.svg";
+
+import "./MainList.scss";
+
+const MainList = (props: { title: string; content: string }) => {
+  const { MainListDumi } = useMainList();
+  const [isMore, setIsMore] = useState<boolean>(false);
+  const [height, setHeight] = useState<string>("165");
+  const handleMore = () => {
+    if (isMore) {
+      setIsMore(false);
+      setHeight("165");
+      console.log(height);
+    } else {
+      setIsMore(true);
+      setHeight(String(85 * MainListDumi.length));
+      console.log(height);
+    }
+  };
+
+  return (
+    <div className="mainList">
+      <div className="mainList-title">
+        <div>{props.title}</div>
+        <div className="mainList-title-button" onClick={() => handleMore()}>
+          {isMore ? "간략히" : "자세히"}
+        </div>
+      </div>
+      <div className="mainList-content" style={{ height: `${height}px` }}>
+        {MainListDumi.map((data) => {
+          return (
+            <>
+              <MainListItem name={data.name} price={data.price} />
+            </>
+          );
+        })}
+      </div>
+      <div className="mainListItem">
+        <div className="mainListItem-item">
+          <div className="mainList-icon">
+            <img src={Add} alt="" />
+          </div>
+          <div className="mainList-text">{props.content}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainList;
