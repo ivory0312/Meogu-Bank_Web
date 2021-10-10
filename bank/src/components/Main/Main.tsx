19b358202949fdcfc741b3cabed0486e0b178673
ivory0312 <63035523+ivory0312@users.noreply.github.com>
2021-10-11 01:09:26 +0900

    Add: main list animation

--- bank/src/components/Main/Main.tsx
+++ bank/src/components/Main/Main.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import MainListItem from "./MainListItem";
 import useMainList from "hooks/main/useMainList";
-import { RiMore2Line } from "react-icons/ri";
 
 import "./Main.scss";
 
@@ -9,39 +8,42 @@ const Main = (): JSX.Element => {
   const { MainListDumi } = useMainList();
   const [isMore, setIsMore] = useState<boolean>(false);
   let count: number = 0;
+  let height: string = "";
   const handleMore = () => {
     if (isMore) {
       setIsMore(false);
+      height = "335";
     } else {
       setIsMore(true);
+      height = String(85 * MainListDumi.length);
     }
   };
 
   return (
-    <div className="main">
-      <div className="main-list">
-        <div className="main-list-title">계좌</div>
-        <div className="main-list-content">
-          <input id="sidebar" type="checkbox" />
-          <div className="sidebar">
-            {MainListDumi.map((data) => {
-              // if (count !== 4) {
-              //   count += 1;
-              // } else {
-              //   return;
-              // }
-              return (
-                <>
-                  <MainListItem name={data.name} price={data.price} />
-                </>
-              );
-            })}
-          </div>
-          <div className="on-off">
-            <label htmlFor="sidebar">더보기</label>
-          </div>
-
-          {/* <div className={isMore ? "isMore" : "isntMore"}>
+    <>
+      <style></style>
+      <div className="main">
+        <div className="main-list">
+          <div className="main-list-title">계좌</div>
+          <div className="main-list-content">
+            <input id="sidebar" type="checkbox" />
+            <div className="on-off">
+              <label htmlFor="sidebar">더보기</label>
+            </div>
+            <div className="sidebar">
+              {MainListDumi.map((data) => {
+                // if (count !== 4) {
+                //   count += 1;
+                // } else {
+                //   return;
+                // }
+                return (
+                  <>
+                    <MainListItem name={data.name} price={data.price} />
+                  </>
+                );
+              })}
+              {/* <div className={isMore ? "isMore" : "isntMore"}>
             {MainListDumi.slice(4).map((data) => {
               return (
                 <>
@@ -51,16 +53,17 @@ const Main = (): JSX.Element => {
               })}
               <div onClick={() => handleMore()}>간략히</div>
             </div> */}
-        </div>
-        {/* <div
+            </div>
+            {/* <div
           className={!isMore ? "isMore" : "isntMore"}
           onClick={() => handleMore()}
-          >
+        >
           더보기
         </div> */}
-        {/* <input className="a" type="checkbox" /> */}
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
