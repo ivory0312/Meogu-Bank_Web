315e9334df7f10b2ff486d1cce5dc40f423fc61c
ivory0312 <63035523+ivory0312@users.noreply.github.com>
2021-10-21 20:37:22 +0900

    Add: default profile image dark mode

--- bank/src/components/Main/Main.tsx
+++ bank/src/components/Main/Main.tsx
@@ -1,15 +1,19 @@
 import MainList from "./MainList/MainList";
-import defaultProfile from "assets/defaultProfile.svg";
+import IDefaultProfile from "assets/IDefaultProfile";
+import { useRecoilValue } from "recoil";
+import { darkModeState } from "recoil/darkMode";
 
 import "./Main.scss";
 
 const Main = (): JSX.Element => {
+  const darkMode = useRecoilValue<boolean>(darkModeState);
+
   return (
     <>
       <div className="main">
         <div className="main-profile">
           <div className="main-profile-image">
-            <img src={defaultProfile} alt="defaultProfile" />
+            <IDefaultProfile color={darkMode ? "#F1F1F3" : "#555555"} />
           </div>
           <div className="main-profile-name">
             <span>박상아</span> 님, <br />
