d032422e6bf1ecd3bebee47d6104818fdc2470f4
ivory0312 <63035523+ivory0312@users.noreply.github.com>
2021-10-21 11:27:20 +0900

    Del: side bar

--- bank/src/components/Common/PageTemplate/PageTemplate.tsx
+++ bank/src/components/Common/PageTemplate/PageTemplate.tsx
@@ -1,14 +1,10 @@
 import { ReactNode } from "react";
-import Side from "../Side";
 
 import "./PageTemplate.scss";
 
 const PageTemplate = (children: { children: ReactNode }): JSX.Element => {
   return (
     <div className="pageTemplate">
-      <div className="pageTemplate-side">
-        <Side />
-      </div>
       <div className="pageTemplate-contents">{children.children}</div>
     </div>
   );
