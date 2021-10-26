5a0f9d681612d2dd6ce9d986c0667cbad29adaae
ivory0312 <63035523+ivory0312@users.noreply.github.com>
2021-10-26 20:57:40 +0900

    Add: main list dumi data idx

--- bank/src/components/Account/Account.tsx
+++ bank/src/components/Account/Account.tsx
@@ -3,8 +3,10 @@ import "./Account.scss";
 const Account = () => {
   return (
     <div className="account">
-      <div className="account-title">계좌 이체</div>
-      <div className="account-content">hi</div>
+      <div className="account-item">
+        <div className="account-item-title">계좌 이체</div>
+        <div className="account-item-content">hi</div>
+      </div>
     </div>
   );
 };
--- bank/src/hooks/main/useMainList.ts
+++ bank/src/hooks/main/useMainList.ts
@@ -1,48 +1,56 @@
 const useMainList = () => {
   const MainListDumi = [
     {
+      idx: 1,
       name: "계좌 1",
       price: 13000,
       image:
         "https://cdn.discordapp.com/attachments/886128260256264227/901298700511506492/unknown.png",
     },
     {
+      idx: 2,
       name: "계좌 2",
       price: 14000,
       image:
         "https://cdn.discordapp.com/attachments/886128260256264227/901298700511506492/unknown.png",
     },
     {
+      idx: 3,
       name: "계좌 3",
       price: 15000,
       image:
         "https://cdn.discordapp.com/attachments/886128260256264227/901298700511506492/unknown.png",
     },
     {
+      idx: 4,
       name: "계좌 4",
       price: 10000,
       image:
         "https://cdn.discordapp.com/attachments/886128260256264227/901298700511506492/unknown.png",
     },
     {
+      idx: 5,
       name: "계좌 5",
       price: 13000,
       image:
         "https://cdn.discordapp.com/attachments/886128260256264227/901298700511506492/unknown.png",
     },
     {
+      idx: 6,
       name: "계좌 6",
       price: 14000,
       image:
         "https://cdn.discordapp.com/attachments/886128260256264227/901298700511506492/unknown.png",
     },
     {
+      idx: 7,
       name: "계좌 7",
       price: 15000,
       image:
         "https://cdn.discordapp.com/attachments/886128260256264227/901298700511506492/unknown.png",
     },
     {
+      idx: 8,
       name: "계좌 8",
       price: 10000,
       image:
